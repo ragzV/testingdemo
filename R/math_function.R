5497e41150b4c874772d065000e645d060c2a700
ragzV <raghavvashisht194@gmail.com>
2026-04-02 13:16:18 -0700

    Added functions and tests

---
+++ R/math_function.R
@@ -0,0 +1,19 @@
+#' Title
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+add_numbers <- function(a,b){
+  return(a+b)
+}
+
+#' Title
+#'
+#' @returns multiplied numbers
+#' @export
+#'
+#' @examples
+multiply_numbers <- function(a,b){
+  return(a*b)
+}
